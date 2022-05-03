569d47ab6ceccf9d9bed131cebafba9389784e6d
Николай Стригачев <nikolai7832@yandex.ru>
2022-05-03 10:02:19 +0300

    Добавьте файлы проекта.

---
+++ AlgoritmAndSheme/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace AlgoritmAndDiagram
+{
+    internal class Program
+    {
+       static string login;
+       static User CurrentUser;
+
+       public static List<User> users = new List<User> { User.Alexandr, User.Ivan };
+
+       static void Main()
+        {
+            Console.WriteLine("Логины доля проверки: \nAlexandr123\tVanya123 \nВведите логин:");
+
+            login = Console.ReadLine();
+
+            // Ищем в листе пользователей пользователя с введенным логином
+            try
+            {
+                CurrentUser = users.First(user => user.Login == login);
+            }
+
+            // Не нашли пользователя, снова переходим к вводу логина
+            catch
+            {
+                Console.WriteLine("Пользователь не найден");
+                Main();
+            }
+
+            if (!CurrentUser.IsPremium)
+            {
+                ShowAds();
+            }
+
+            Console.ReadLine();
+        }
+
+       static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ AlgoritmAndSheme/User.cs
@@ -0,0 +1,28 @@
+﻿namespace AlgoritmAndDiagram
+{
+    // Класс содержит в себе данные пользователей
+    public class User
+    {
+        public string Login { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsPremium { get; set; }
+
+
+        public static User Ivan = new User
+        {
+            Login = "Vanya123",
+            Name = "Ivan",
+            IsPremium = true,
+        };
+
+        public static User Alexandr = new User
+        {
+            Login = "Alexandr123",
+            Name = "Alexandr",
+            IsPremium = false,
+        };
+
+    }
+}
